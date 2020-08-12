4599dca208078ea05f864cc26a2ace8b4045705f
GRomanDev <grromf@gmail.com>
2020-08-13 00:04:33 +0800

    connected navbar-dropdown

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  var headerMenu = $(".header-menu");
+  headerMenu.on("click", function () {
+    $(".navbar-dropdown").toggleClass("navbar-dropdown--visible");
+  });
+});
