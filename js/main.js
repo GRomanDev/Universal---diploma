f38eff1bc2b065c36df30ec73a3d72aa47ea784a
GRomanDev <grromf@gmail.com>
2020-08-14 02:39:40 +0800

    create: connected tabs by js

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(document).ready(function () {
     $(".navbar-dropdown").toggleClass("navbar-dropdown--visible");
   });
   $(".fancybox").fancybox();
+
+  const tab = $(".tab");
+  tab.on("click", function () {
+    tab.removeClass("tabs-aside__item--active");
+    $(this).toggleClass("tabs-aside__item--active");
+    let activeTabContent = $(this).attr("data-target");
+    $(".tabs-content").removeClass("tabs-content--visible");
+    $(activeTabContent).toggleClass("tabs-content--visible");
+  });
 });
